e91d5f39af303e27ce4f9b7f2b46bffd046f11b2
kimbakker <39679854+kimbakker@users.noreply.github.com>
2019-11-30 09:48:35 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
 function uitklapMenu() {
     document.querySelector('.menu-categorie').classList.toggle('uitklapeffect1');
+    document.querySelector('.fa-chevron-down').classList.toggle('fa-rotate-180');
 }
 
 document.querySelector('.categorie-titel').addEventListener("click", uitklapMenu);
+
+function uitklapMenuMuziek() {
+    document.querySelector('.menu-categorie-muziek').classList.toggle('uitklapeffect2');
+    document.querySelector('.sub').classList.toggle('fa-rotate-180');
+}
+
+document.querySelector('.muziek').addEventListener("click", uitklapMenuMuziek);
+
+
+
+
